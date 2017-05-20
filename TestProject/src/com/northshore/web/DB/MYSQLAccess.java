d3930b23a690ce2b79eba0c02e7afb5fe86cb155
Prasanga <prasangafdz@outlook.com>
2017-05-20 14:02:09 +0530

    It's connecting to the DB. (No password for the database)

--- TestProject/src/com/northshore/web/DB/MYSQLAccess.java
+++ TestProject/src/com/northshore/web/DB/MYSQLAccess.java
@@ -1,8 +1,29 @@
 package com.northshore.web.DB;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+
 /**
  * Created by 150101002 on 5/20/2017.
  */
 public class MYSQLAccess {
+    private Connection connect;
+public void readDatabase() throws Exception {
+    try {
+        Class.forName("com.mysql.jdbc.Driver");
 
+        connect = DriverManager
+                .getConnection("jdbc:mysql://localhost/AIRPORT_SYS?"
+                + "user=root");
+    }
+    catch (Exception ex){
+        ex.printStackTrace();
+
+    }
+}
+
+    public static void main(String[] args) throws Exception {
+        MYSQLAccess access = new MYSQLAccess();
+        access.readDatabase();
+    }
 }
